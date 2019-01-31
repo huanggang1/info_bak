8e836338e229fb8c7c70f03089e0cf1dcf1b9a53
Your Name <you@example.com>
2019-01-31 15:31:22 +0800

    用户详情

---
+++ resources/views/admin/user/detail.blade.php
@@ -0,0 +1,37 @@
+@extends('admin.layouts.base')
+
+@section('title','控制面板')
+
+@section('pageHeader','控制面板')
+
+@section('pageDesc','DashBoard')
+
+@section('content')
+    <div class="main animsition">
+        <div class="container-fluid">
+
+            <div class="row">
+                <div class="">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">查看用户</h3>
+                        </div>
+                        <div class="panel-body">
+
+                            @include('admin.partials.errors')
+                            @include('admin.partials.success')
+                            <div class="form-horizontal">
+                                @include('admin.user._form')
+                                <div class="form-group">
+                                    <div class="col-md-7 col-md-offset-3">
+                                       <a class="btn btn-primary btn-md" href="/admin/user/" >返回</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
