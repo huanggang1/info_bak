fd9c0a8f3b0c4b04fd7030aaa71ebe855809f1c5
Your Name <you@example.com>
2019-01-31 14:01:15 +0800

    删除缓存文件

--- storage/framework/views/45fcdb86bdea6459577e27db7cdd571a02c460b1.php
+++
@@ -1,8 +0,0 @@
-<div class="item">
-    <label class="lable"  for="tag" >负责 人：</label><input type="text" name="person"  value="<?php echo e($infoData['person']); ?>" <?php echo e($readonly); ?> autofocus>
-    <label class="lable"  for="tag" >介绍 人：</label><input type="text"  name="introducer" value="<?php echo e($infoData['introducer']); ?>" <?php echo e($readonly); ?> autofocus>
-</div><br/>
-<div class="item">
-  <label class="lable"  for="tag" >备注：</label>
-  <textarea name="remarks"  <?php echo e($readonly); ?> ><?php echo e($infoData['remarks']); ?></textarea>
-</div>
